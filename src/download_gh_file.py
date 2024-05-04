96d08ae306107afa2f4d026683e885f5c7031fbf
Aditya Agarwal <adityag2511@gmail.com>
2024-05-04 01:29:20 -0700

    [download_gh_file] Using PathLib instead of os for removing file

--- src/download_gh_file.py
+++ src/download_gh_file.py
@@ -2,14 +2,27 @@ from datetime import datetime, timedelta, timezone
 import requests
 import gzip
 import shutil
-import os
+from pathlib import Path
 
 def download_file_from_url(url, target):
 	response = requests.get(url)
 	if response.ok and response.status_code == 200:
 		with open(target, 'wb') as output_file:
 			output_file.write(response.content)
-	return response.status_code
+		return target
+	else:
+		print(response.content)
+
+def download_gh_file_from_date(date, extract=True):
+	gh_file_name = f'{date.date()}-{date.hour}.json.gz'
+	gh_file_url = f'https://data.gharchive.org/{gh_file_name}'
+	new_file_download_path = f'../data/raw_gh_data/{gh_file_name}'
+	downloaded_file = download_file_from_url(gh_file_url, new_file_download_path)
+	if extract and downloaded_file:		
+		data_file_path = new_file_download_path.rsplit('.',1)[0]
+		uncompress_gz_file(new_file_download_path, data_file_path)
+		return data_file_path
+	return downloaded_file
 
 def get_latest_gh_file_date():
 	return datetime.now(timezone.utc) - timedelta(hours=1)
@@ -19,14 +32,8 @@ def uncompress_gz_file(gz_file, target_file, remove_original=True):
 		with open(target_file, 'wb') as f_out:
 			shutil.copyfileobj(f_in, f_out)
 	if remove_original:
-		os.remove(gz_file)
+		Path.unlink(gz_file)
 
 if __name__ == '__main__':
 	latest_gh_file_date = get_latest_gh_file_date()
-	latest_gh_file_name = f'{latest_gh_file_date.date()}-{latest_gh_file_date.hour}.json.gz'
-	latest_gh_file_url = f'https://data.gharchive.org/{latest_gh_file_name}'
-	new_file_download_path = f'../data/raw_gh_data/{latest_gh_file_name}'
-	data_file_path = new_file_download_path.rsplit('.',1)[0]
-
-	download_file_from_url(latest_gh_file_url, new_file_download_path)
-	uncompress_gz_file(new_file_download_path, data_file_path)
+	new_file_path = download_gh_file_from_date(latest_gh_file_date)
