89433a031910e64953f5c1a4a6d7be7b7798cc6a
Aditya Agarwal <adityag2511@gmail.com>
2024-05-07 23:21:44 -0700

    [DownloadGhFile] Logging File Download and fixing Path Lib parameter type issues

--- src/download_gh_file.py
+++ src/download_gh_file.py
@@ -17,7 +17,7 @@ def download_file_from_url(url, target):
 def download_gh_file_from_date(date, extract=True):
 	gh_file_name = f'{date.date()}-{date.hour}.json.gz'
 	gh_file_url = f'https://data.gharchive.org/{gh_file_name}'
-	new_file_download_path = f'../data/raw_gh_data/{gh_file_name}'
+	new_file_download_path = f'../data/raw_gh_data/{date.date()}/{gh_file_name}'
 	downloaded_file = download_file_from_url(gh_file_url, new_file_download_path)
 	if extract and downloaded_file:		
 		data_file_path = new_file_download_path.rsplit('.',1)[0]
@@ -26,14 +26,15 @@ def download_gh_file_from_date(date, extract=True):
 	return downloaded_file
 
 def get_latest_gh_file_date():
-	return datetime.now(timezone.utc) - timedelta(hours=1)
+	now = datetime.now(timezone.utc).replace(minute=0, second=0, microsecond=0)
+	return now - timedelta(hours=1)
 
 def uncompress_gz_file(gz_file, target_file, remove_original=True):
 	with gzip.open(gz_file, 'rb') as f_in:
 		with open(target_file, 'wb') as f_out:
 			shutil.copyfileobj(f_in, f_out)
 	if remove_original:
-		Path.unlink(gz_file)
+		Path.unlink(Path(gz_file))
 
 if __name__ == '__main__':
 	latest_gh_file_date = get_latest_gh_file_date()
