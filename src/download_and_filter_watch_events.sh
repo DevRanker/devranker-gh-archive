76cbb8942402bc43309d14423b0b6c5afc439ca5
Aditya Agarwal <adityag2511@gmail.com>
2024-05-08 17:36:20 -0700

    [shell_script][download_n_filter] Explitcitly creating the download directory if not found

--- src/download_and_filter_watch_events.sh
+++ src/download_and_filter_watch_events.sh
@@ -7,6 +7,7 @@ file_hour=$2
 file_date_n_hour="${file_date}-${file_hour}"
 gz_filename="${file_date_n_hour}.json.gz"
 download_dir="../data/raw_gh_data/${file_date}"
+mkdir -p $download_dir
 wget -P $download_dir "https://data.gharchive.org/${gz_filename}"
 
 dest_dir="../data/watch_event_data/hourly/${file_date}"
